0f84f832b075449811a6d8d68c1b0f00ecf9a032
Tony Landschoot <tony.landschoot@outlook.com>
2020-08-21 13:50:35 +0200

    fix test

---
+++ src/test/java/com/api/finorgback/controllers/OperationControllerTest.java
@@ -0,0 +1,14 @@
+package com.api.finorgback.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OperationControllerTest {
+
+    @Test
+    void getAllOperationsTest() {
+        assertTrue(true);
+    }
+
+}
